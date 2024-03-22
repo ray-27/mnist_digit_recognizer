7f21c053039f25a62b6e99fb5ef061e5d620a385
Rajveer Yadav <51945519+ray-27@users.noreply.github.com>
2024-03-22 21:25:01 +0530

    updated the drive token

--- drive.py
+++ drive.py
@@ -4,7 +4,7 @@ import requests
 
 def upload(file,filename="model"):
     print(f"uploading {filename} to Drive")
-    header = {"Authorization": "Bearer ya29.a0Ad52N3943f3-VqZBEiOwuUw4Q5c2_5lRFMNhZKpMRdNbvleGG4oZp1xIbGbPpWJe9JsRR_HKMap922KEEg6ziwLiOwsnmnT7ysN8SfpCwXmffJdE237hmI_j558fgvaTI68bbBAimaUcHkDbfCO4S27A6R51xG9RQ_4ZaCgYKAWQSARASFQHGX2Mi-LMjJWP0wIxNLPMyfblwWg0171"}
+    header = {"Authorization": "Bearer ya29.a0Ad52N3-rPYFJtA8H3cKnd49CH_A6_7jQPn7WwwUdESoVr5bIXzPRw8mbZCRtzyWCQH_OZf-EXDeNSrT51Wz82BSRWXz6JgY5eQ3hxbmW85r9Jc0UeYIpmwCeCeBL0EwQehfMSEhv65621VIkEYXSFa2JoPn7GSg7oiYlaCgYKAc8SARASFQHGX2MirZLMGlLWarPURyi5FsQY_g0171"}
 
     para = {
         "name": filename
@@ -21,6 +21,3 @@ def upload(file,filename="model"):
     )
     print("Upload Complete")
     pass
-
-
-
