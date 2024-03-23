7c786ccccc802a1564646683c193d9f407774ebc
Rajveer Yadav <51945519+ray-27@users.noreply.github.com>
2024-03-23 19:10:37 +0530

    updated the train function

--- train.py
+++ train.py
@@ -16,54 +16,56 @@ from model import mnist_model
 from util import plot_loss
 from drive import upload
 
-Epoches = 100
-Learning_rate = 1e-3
-Batch_size = 4
+# Epoches = 100
+# Learning_rate = 1e-3
+# Batch_size = 4
 
-#configuring the device
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-    print(Fore.GREEN + "Device is CUDA")
-elif torch.backends.mps.is_available():
-    device = torch.device('mps')
-    print(Fore.GREEN + "Device is MPS")
-else:
-    device = torch.device('cpu')
-    print("Device is running on cpu")
+def train(Epoches = 100,Learning_rate = 1e-4,Batch_size = 4):
 
-#preparing the dataloader
-custom_dataloader = DataLoader(mnist_data(), batch_size=Batch_size, shuffle=True,pin_memory=True)
+    #configuring the device
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print(Fore.GREEN + "Device is CUDA")
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+        print(Fore.GREEN + "Device is MPS")
+    else:
+        device = torch.device('cpu')
+        print("Device is running on cpu")
 
-#preparing the model
-model = mnist_model().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=Learning_rate)
+    #preparing the dataloader
+    custom_dataloader = DataLoader(mnist_data(), batch_size=Batch_size, shuffle=True,pin_memory=True)
 
-# Train the model
-i = 0
-loss_list = []
-for epoch in tqdm(range(Epoches)):
+    #preparing the model
+    model = mnist_model().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=Learning_rate)
 
-    model.train()
+    # Train the model
+    i = 0
+    loss_list = []
+    for epoch in tqdm(range(Epoches)):
 
-    for data, target in custom_dataloader:
-        data = data.to(device)
-        target = target.to(device)
+        model.train()
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+        for data, target in custom_dataloader:
+            data = data.to(device)
+            target = target.to(device)
 
-    loss_list.append(loss.item())
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
 
-torch.save(model.state_dict(), 'mnist_classifier.pt')
+        loss_list.append(loss.item())
 
-try:
-    upload('mnist_classifier.pt',"Mnist model")
-except: print(Fore.RED + "Could not upload the model to the drive")
+    torch.save(model.state_dict(), 'mnist_classifier.pt')
 
-try:
-    plot_loss(loss_list,save=True)
-except: print("could not upload the plot to drive")
+    try:
+        upload('mnist_classifier.pt',"Mnist model")
+    except: print(Fore.RED + "Could not upload the model to the drive")
+
+    try:
+        plot_loss(loss_list,save=True)
+    except: print("could not upload the plot to drive")
